b1f67ad7aabb72e0ede15d3eeb3d1537c45e9040
haruto2223 <henrymawlon50@gmail.com>
2023-04-03 04:22:00 +0900

    Insert live account

---
+++ src/pages/LiveAccount.jsx
@@ -0,0 +1,15 @@
+import Navbar from "../components/dashboard/Navbar";
+
+const LiveAccount = () => {
+    return (
+        <div className="bg-slate-200">
+            <Navbar/>
+            <p className="text-center text-6xl text-[#13BE00] py-10">OPEN A LIVE ACCOUNT</p>
+            <div className="mx-auto w-1/2 rounded-3xl shadow-xl bg-white">
+                
+            </div>
+        </div>
+    )
+}
+
+export default LiveAccount;
