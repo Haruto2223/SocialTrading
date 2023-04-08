43c7b468ab6cc1a8f9944d27b0d5b000245da314
haruto2223 <henrymawlon50@gmail.com>
2023-04-08 14:29:26 -0400

    web api first milestone

--- server/models/Follower.js
+++ server/models/Follower.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema();
 
 const FollowerSchema = new Schema({
-    client: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'client'
+    accountID: {
+        type: Number,
+        require: true
     },
     provider: {
         type: mongoose.Schema.Types.ObjectId,
--- server/routes/trader.js
+++ server/routes/trader.js
@@ -24,13 +24,13 @@ router.get('/provider/all', async(req, res) => {
 
 //Provider register
 router.post('/provider', async(req, res) => {
-    const {server, id, password, nickName, fee} = req;
+    const {server, id, password, nickName, fee} = req.body;
     try{
         //api operation
         const clients = await api.providerRegister(server, id, password, fee);
         clients.map(async(client) => {
             try{
-                let provider = await Provider.findOne({ nickName });
+                let provider = await Provider.findOne({ nickName: client.nickName });
 
                 if (provider) {
                   return res
@@ -46,7 +46,7 @@ router.post('/provider', async(req, res) => {
                 });         
                 await provider.save();
             } catch(err){
-                
+                return res.status(400).json({errors: [{ msg: 'failed to verify your account'}] })
             }
         });        
         res.json(clients);
@@ -59,22 +59,74 @@ router.post('/provider', async(req, res) => {
 //Follower register
 router.post('/follower', async(req, res) => {
     const {id, server, password, providerNickname, strategy} = req.body;
+    try{
+        //api operation
+        const clients = await api.followerRegister(server, id, password, strategy, providerNickname);
+        clients.map(async(client) => {
+            try{
+                let follower = await Follower.findOne({accountID: client.id});
+
+                if (follower) {
+                  return res
+                    .status(400)
+                    .json({ errors: [{ msg: 'Provider already exists' }] });
+                }
+          
+                follower = new Follower({
+                  server,
+                  nickName,
+                  fee,
+                  id
+                });         
+                await follower.save();
+            } catch(err){
+                return res.status(400).json({errors: [{ msg: 'failed to verify your account'}] })
+            }
+        });        
+        res.json(clients);
+    } catch(err)
+    {
+
+    }
 })
 
 //Provider || Follower login
 router.post('/login', async(req, res) => {
-    const {server, id, password, strategy} = req.body;
+    const {server, id, password, category} = req.body;
+    try{
+        //api operation
+        const clients = await api.login(server, id, password, category);      
+        res.json(clients);
+    } catch(err)
+    {
+
+    }
 })
 
 //Get all of my providers
 router.get('/follower/:id', async(req, res) => {
     const id = req.params.id;
+    try{
+        //api operation
+        const clients = await api.getProviders(id);      
+        res.json(clients);
+    } catch(err)
+    {
 
+    }
 })
 
 //Get all of my followers
 router.get('/provider/:id', async(req, res) => {
     const id = req.params.id;
+    try{
+        //api operation
+        const clients = await api.getFollowers(id);      
+        res.json(clients);
+    } catch(err)
+    {
+
+    }
 })
 
 module.exports = router;
